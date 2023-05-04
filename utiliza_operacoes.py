0a2777b7a6e75e72aefced2febff92bec23a58cc
Daniel Freitas <bikemessengerateu@gmail.com>
2023-05-04 16:30:26 -0300

    operacoes
    tarefa de revisão

---
+++ utiliza_operacoes.py
@@ -0,0 +1,13 @@
+from operacoes import *
+
+num1 = 10
+num2 = 3
+
+print("A soma de", num1, "+", num2, "é igual a", soma(num1, num2))
+print("A subtração de", num1, "-", num2, "é igual a", subtracao(num1, num2))
+print("A multiplicação de", num1, "*", num2, "é igual a", multiplicacao(num1, num2))
+print("A divisão de", num1, "/", num2, "é igual a", divisao(num1, num2))
+print("O resto da divisão de", num1, "por", num2, "é igual a", resto(num1, num2))
+print("A divisão inteira de", num1, "por", num2, "é igual a", divisao_inteira(num1, num2))
+
+print("Obrigado por utilizar o programa!")
