fd82fc670d29760c2f95b4b2a4922932ed5d139e
Alex <alexdeividy@hotmail.com>
2023-05-04 16:44:08 -0300

    Adicionado SOMA
    soma ALEX

--- funcoes.py
+++
@@ -1,2 +0,0 @@
-def soma(valor1, valor2):
-    print("A sua soma deu:",valor1 + valor2, "\nObrigado por utilizar o nosso programa!")
--- main.py
+++
@@ -1,6 +0,0 @@
-import funcoes as funcoes
-
-valor1 = float(input("Insira o primeiro valor: "))
-valor2 = float(input("Insira o segundo valor: "))
-funcoes.soma(valor1, valor2)
-
