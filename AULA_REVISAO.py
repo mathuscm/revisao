e2a4018df6828785fe637744ce0a9f9f174f3cda
Matheus S Cordaro <132008218+mathuscm@users.noreply.github.com>
2023-05-04 16:43:50 -0300

    Add files via upload

---
+++ AULA_REVISAO.py
@@ -0,0 +1,17 @@
+def soma(valor1,valor2):
+    return valor1+valor2
+
+def sub(valor1,valor2):
+    return valor1-valor2
+
+def mult(valor1,valor2):
+    return valor1*valor2
+
+def resto(valor1,valor2):
+    return valor1%valor2
+
+def div(valor1,valor2):
+    return valor1/valor2
+
+def divdiv(valor1,valor2):
+    return valor1//valor2
